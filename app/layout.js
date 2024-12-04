031e1e3707cb73e40c7c3036dda115bb26d095e1
bong-suk <greedi9468@hanmail.net>
2024-12-04 15:12:25 +0900

    next13으로 간단한 앱 만들기

---
+++ app/layout.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">홈</Link>
+            </li>
+            <li>
+              <Link href="/dog">강아지</Link>
+            </li>
+            <li>
+              <Link href="/cat">고양이</Link>
+            </li>
+          </ul>
+        </nav>
+        {children}
+      </body>
+    </html>
+  );
+}
